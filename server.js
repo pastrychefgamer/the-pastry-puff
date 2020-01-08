3344463d51edf33c376bd4a405699988dfe68554
William Aleshire <Mine@Williams-iMac.domain>
2020-01-07 16:58:07 -0800

    basic directory structure completed and linked

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const port = 3000; 
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+
+
+const app = express();
+
+// Connect to DB
+require('./config/database');
+
+// Configure Express App app.set()
+app.set('view engine', 'ejs');
+
+// Mount middleware app.use()
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Mount Routes app.use()
+app.use('/', indexRouter);
+
+// Tell App to listen
+app.listen(port, () => {
+    console.log(`Express is listening on port:${port}`);
+});
